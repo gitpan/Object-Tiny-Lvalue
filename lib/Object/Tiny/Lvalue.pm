0766fdddcab6dde7660ac5f48d54f8f69196bd3b
Αριστοτέλης Παγκαλτζής <pagaltzis@gmx.de>
2010-06-05 07:14:56 +0000

    Import of ARISTOTLE/Object-Tiny-Lvalue-1.07 from CPAN.
    gitpan-cpan-distribution: Object-Tiny-Lvalue
    gitpan-cpan-version:      1.07
    gitpan-cpan-path:         ARISTOTLE/Object-Tiny-Lvalue-1.07.tar.gz
    gitpan-cpan-author:       ARISTOTLE
    gitpan-cpan-maturity:     released

--- lib/Object/Tiny/Lvalue.pm
+++ lib/Object/Tiny/Lvalue.pm
@@ -1,10 +1,14 @@
 package Object::Tiny::Lvalue;
+BEGIN {
+  $Object::Tiny::Lvalue::VERSION = '1.07';
+}
 
-use strict; no strict 'refs';
+# ABSTRACT: minimal class builder with lvalue accessors
+
+use strict 'vars';
 
 BEGIN {
 	require 5.003_96;
-	$Object::Tiny::Lvalue::VERSION = '1.06';
 }
 
 sub import {
@@ -30,11 +34,17 @@ sub new {
 
 1;
 
-__END__
+
+
+=pod
 
 =head1 NAME
 
-Object::Tiny::Lvalue - Minimal class builder with lvalue accessors
+Object::Tiny::Lvalue - minimal class builder with lvalue accessors
+
+=head1 VERSION
+
+version 1.07
 
 =head1 SYNOPSIS
 
@@ -55,35 +65,19 @@ Use the class:
 
 This is a clone of L<Object::Tiny|Object::Tiny>, but adjusted to create accessors that return lvalues.
 
-=head1 SUPPORT
-
-Please report any bugs or feature requests at L<http://github.com/ap/Object-Tiny-Lvalue/issues>. For other issues, contact the author.
-
 =head1 AUTHOR
 
-=over 4
+  Aristotle Pagaltzis <pagaltzis@gmx.de>
 
-=item *
+=head1 COPYRIGHT AND LICENSE
 
-Adam Kennedy L<mailto:adamk@cpan.org> for original Object::Tiny.
+This software is copyright (c) 2010 by Aristotle Pagaltzis.
 
-=item *
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
-Aristotle Pagaltzis L<mailto:pagaltzis@gmx.de> for the Object::Tiny::Lvalue variant.
-
-=back
-
-=head1 COPYRIGHT
-
-Copyright (c) 2007--2008 Adam Kennedy.
-
-Copyright (c) 2009 Aristotle Pagaltzis.
-
-This module is free software; you can redistribute it and/or modify it under the same terms as Perl itself. See L<perlartistic>.
+=cut
 
 
-=head1 DISCLAIMER OF WARRANTY
+__END__
 
-BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR, OR CORRECTION.
-
-IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
